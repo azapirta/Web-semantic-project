9d362d02617a2a3fc836802ffb63754d154a4d91
azapirta <zapirtanandrei@yahoo.com>
2020-04-16 18:29:01 +0300

    Start

--- principal.php
+++ principal.php
@@ -3,9 +3,6 @@
 		<title>Reprezentanta auto</title>
 	</head>
 	<body>
-		<h1>Modificari</h1>
-		<button value = "Afiseaza categorii"></button>
-		<h3>Reprezentam Cluj-Napoca</h3>
-		<h4>titlu</h4>
+		<button value = "Afiseaza categorii">
 	</body>
 </html>
