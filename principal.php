df472c99071d54b7c0859e3f5acaab65c83bf443
azapirta <zapirtanandrei@yahoo.com>
2020-04-16 18:40:09 +0300

    fa

--- principal.php
+++ principal.php
@@ -4,7 +4,7 @@
 	</head>
 	<body>
 		<input type = "button" value = "Afiseaza categorii">
-		<div>
+		<div style = "weight: 100%; height: 51%">
 			<div id = "zonaA" style = "weight: 29%; height: 50%; border: 1px; border-color:black">
 			</div>
 			<div id = "zonaB" style = "weight: 69%; height: 50%; border: 1px border-color:black">
